25c88cf0c042ca74c6b1fb0f67d4d1e8e34090c9
Christopher Pickering <christopher@going.bg>
2023-01-12 10:05:10 -0600

    fixed pagination

--- main.py
+++ main.py
@@ -28,25 +28,30 @@ if os.environ.get("CHUNK"):
 def get_urls():
 
     page_size=100
-    page = (START/page_size) + 1
+    page = START
 
-    end_page = (END/page_size)
+    end_page = END
 
     URLS=[]
 
     while True:
         p = requests.get(f'https://api.joshuaproject.net/v1/people_groups.json?api_key={API_KEY}&page={page}&limit={page_size}')
 
-        data=p.json()
+        try:
+            data=p.json()
 
-        for group in data:
-            if "Resources" not in group:
-                continue
-            for resource in group["Resources"]:
-                if "URL" in resource:
-                    URLS.append(resource["URL"])
+            for group in data:
+                if "Resources" not in group:
+                    continue
+                for resource in group["Resources"]:
+                    if "URL" in resource:
+                        URLS.append(resource["URL"])
 
-        yield page, [*set(URLS)]
+            yield page, [*set(URLS)]
+
+        except BaseException as e:
+            print(f"Error with https://api.joshuaproject.net/v1/people_groups.json?api_key=...&page={page}&limit={page_size}'")
+            print(e)
 
         # if page size is < 100 then we reached the end.
         if len(data) < 100 or page == end_page:
@@ -88,26 +93,15 @@ def runner():
         for row in url_errors:
             writer.writerow(row)
 
-    # if there were any 999 or 400 or 406 return an error.
-    # get all status codes
-    codes = [x for x in url_errors if x[1] in [999,400,406]]
-
-    if codes:
-        exit= 1
-    exit =0
-
-    return pages, total, exit
+    return pages, total
 
 if __name__ == '__main__':
     start_time = time.time()
-    pages, total,exit = runner()
+    pages, total = runner()
 
     duration = time.time() - start_time
     print(f"Processed {total} urls in {duration} seconds from {int(pages)} API requests")
 
-    if exit==1:
-        # will return error
-        sys.exit()
 
 
 
